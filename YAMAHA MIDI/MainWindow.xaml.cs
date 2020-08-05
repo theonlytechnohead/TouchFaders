4b77bb00e8f8b97fe13448d39447478852dec5ae
Craig Anderson <cranderson0110@gmail.com>
2020-08-05 15:37:43 +1200

    Continued implementation
    Progress made!

--- YAMAHA MIDI/MainWindow.xaml.cs
+++ YAMAHA MIDI/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace YAMAHA_MIDI {
 		InputDevice LS9_in;
 		OutputDevice LS9_out;
 
+		InputDevice DAW_in;
+		OutputDevice DAW_out;
+
 		public MainWindow () {
 			InitializeComponent();
 			InitializeIO();
@@ -30,8 +33,24 @@ namespace YAMAHA_MIDI {
 		}
 
 		public void InitializeIO () {
-			LS9_in = new InputDevice(0);
-			LS9_out = new OutputDevice(0);
+			LS9_in = new InputDevice(0); // Make sure to get and set properly as defined by GUI later
+			LS9_in.SysExMessageReceived += HandleSysExMessage;
+			LS9_out = new OutputDevice(0); // As above
+
+			DAW_in = new InputDevice(1); // Ditto
+			DAW_in.ChannelMessageReceived += HandleChannelMessage;
+			DAW_out = new OutputDevice(1); // ''
+		}
+
+		private void HandleChannelMessage (object sender, ChannelMessageEventArgs e) {
+			ChannelMessage channelMessage = e.Message;
+
+			// Convert the channel and value and send the correct SysEx command to desk
+		}
+
+		private void HandleSysExMessage (object sender, SysExMessageEventArgs e) {
+			SysExMessage sysExMessage = e.Message;
+			// Now respond if the message matches the right message format for the CH 1-16 send level for MIX 1-6 to DAW_out
 		}
 
 		public void TestOutput () {
@@ -54,6 +73,13 @@ namespace YAMAHA_MIDI {
 			LS9_out.Send(builder.Result);
 
 
+			ChannelMessageBuilder ch1mix10db = new ChannelMessageBuilder();
+			ch1mix10db.Command = ChannelCommand.Controller; // Control change?
+			ch1mix10db.MidiChannel = 0;
+			ch1mix10db.Data1 = 1; // idk about this one really, I think maybe the CC number?
+			ch1mix10db.Data1 = 127; // Value, a.k.a. volume I think
+			ch1mix10db.Build();
+			DAW_out.Send(ch1mix10db.Result);
 		}
 	}
 }
