96aa676ef45271d5126cf864eefa0abec7534d48
Craig Anderson <cranderson0110@gmail.com>
2020-08-05 22:24:43 +1200

    Almost kinda working a bit

--- YAMAHA MIDI/MainWindow.xaml.cs
+++ YAMAHA MIDI/MainWindow.xaml.cs
@@ -12,7 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Sanford.Multimedia.Midi;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Devices;
 
 namespace YAMAHA_MIDI {
 	/// <summary>
@@ -20,66 +22,50 @@ namespace YAMAHA_MIDI {
 	/// </summary>
 	public partial class MainWindow : Window {
 
-		InputDevice LS9_in;
-		OutputDevice LS9_out;
-
-		InputDevice DAW_in;
-		OutputDevice DAW_out;
-
 		public MainWindow () {
 			InitializeComponent();
 			InitializeIO();
-			TestOutput();
+		}
+
+		protected override void OnClosed (EventArgs e) {
+
+			base.OnClosed(e);
 		}
 
 		public void InitializeIO () {
-			LS9_in = new InputDevice(0); // Make sure to get and set properly as defined by GUI later
-			LS9_in.SysExMessageReceived += HandleSysExMessage;
-			LS9_out = new OutputDevice(0); // As above
+			foreach (var outputDevice in OutputDevice.GetAll()) {
+				Console.WriteLine(outputDevice.Name);
+			}
+			foreach (var inputDevice in InputDevice.GetAll()) {
+				inputDevice.EventReceived += DAW_out_EventReceived;
+				try {
+					inputDevice.StartEventsListening();
+				} catch {
+					continue;
+				}
 
-			DAW_in = new InputDevice(1); // Ditto
-			DAW_in.ChannelMessageReceived += HandleChannelMessage;
-			DAW_out = new OutputDevice(1); // ''
+			}/*
+			using (var DAW_out = InputDevice.GetByName("DAW_out")) {
+				DAW_out.EventReceived += DAW_out_EventReceived;
+				DAW_out.StartEventsListening();
+			}*/
 		}
 
-		private void HandleChannelMessage (object sender, ChannelMessageEventArgs e) {
-			ChannelMessage channelMessage = e.Message;
-
-			// Convert the channel and value and send the correct SysEx command to desk
+		private void DAW_out_EventReceived (object sender, MidiEventReceivedEventArgs e) {
+			var DAW_out = (MidiDevice)sender;
+			Console.WriteLine($"Event received from '{DAW_out.Name}' at {DateTime.Now}: {e.Event}");
 		}
 
-		private void HandleSysExMessage (object sender, SysExMessageEventArgs e) {
-			SysExMessage sysExMessage = e.Message;
-			// Now respond if the message matches the right message format for the CH 1-16 send level for MIX 1-6 to DAW_out
+		private void TestOutput () {
+			using (var DAW_in = OutputDevice.GetByName("DAW")) {
+				SevenBitNumber cc = new SevenBitNumber(0); // 0-127
+				SevenBitNumber value = new SevenBitNumber(97); // 0dB is right between 96 and 97 in REAPER
+				DAW_in.SendEvent(new ControlChangeEvent(cc, value));
+			}
 		}
 
-		public void TestOutput () {
-			ChannelMessageBuilder builder = new ChannelMessageBuilder();
-
-			builder.Command = ChannelCommand.NoteOn;
-			builder.MidiChannel = 0;
-			builder.Data1 = 60;
-			builder.Data2 = 127;
-			builder.Build();
-
-			LS9_out.Send(builder.Result);
-
-			Thread.Sleep(1000);
-
-			builder.Command = ChannelCommand.NoteOff;
-			builder.Data2 = 0;
-			builder.Build();
-
-			LS9_out.Send(builder.Result);
-
-
-			ChannelMessageBuilder ch1mix10db = new ChannelMessageBuilder();
-			ch1mix10db.Command = ChannelCommand.Controller; // Control change?
-			ch1mix10db.MidiChannel = 0;
-			ch1mix10db.Data1 = 1; // idk about this one really, I think maybe the CC number?
-			ch1mix10db.Data1 = 127; // Value, a.k.a. volume I think
-			ch1mix10db.Build();
-			DAW_out.Send(ch1mix10db.Result);
+		private void sendMIDI_Click (object sender, RoutedEventArgs e) {
+			TestOutput();
 		}
 	}
 }
