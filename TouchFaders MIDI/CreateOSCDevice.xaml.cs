5f1d0d448636e65aee4b3e2a9af21c55591d0611
Craig Anderson <cranderson0110@gmail.com>
2020-11-06 12:04:50 +1300

    Renamed app!
    Also, removed references to YAMAHA® that violated their trademark(s)

---
+++ TouchFaders MIDI/CreateOSCDevice.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TouchFaders_MIDI {
+	/// <summary>
+	/// Interaction logic for CreateOSCDevice.xaml
+	/// </summary>
+	public partial class CreateOSCDevice : Window {
+		public CreateOSCDevice () {
+			InitializeComponent();
+		}
+
+		private void Window_Loaded (object sender, RoutedEventArgs e) {
+			name.Focus();
+			//addressIPTextBox.FirstSegment.Focus();
+			addressIPTextBox.AddressChangedEvent = textChanged;
+		}
+
+		private void checkValidInfo () {
+			if (addressIPTextBox.hasValidAddress() && sendPort.Text.Length != 0 && listenPort.Text.Length != 0) {
+				addButton.IsEnabled = true;
+			} else {
+				addButton.IsEnabled = false;
+			}
+		}
+
+		private void textChanged (object sender, TextChangedEventArgs e) => checkValidInfo();
+
+		private void addButton_Click (object sender, RoutedEventArgs e) => DialogResult = true;
+
+		private void listenPort_PreviewTextInput (object sender, TextCompositionEventArgs e) => e.Handled = !IsValidPort(((TextBox)sender).Text + e.Text);
+
+		private void sendPort_PreviewTextInput (object sender, TextCompositionEventArgs e) => e.Handled = !IsValidPort(((TextBox)sender).Text + e.Text);
+
+		public static bool IsValidPort (string str) {
+			return int.TryParse(str, out int i) && 0 < i && i <= 65535; // Make sure that the int entered is a valid UDP port
+		}
+
+		private void cancelButton_Click (object sender, RoutedEventArgs e) => Close();
+	}
+}
---
+++ TouchFaders MIDI/Win32.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TouchFaders_MIDI {
+	class Win32 {
+
+		[DllImport("Winmm.dll")]
+		public static extern uint midiInGetNumDevs ();
+
+		[DllImport("Winmm.dll")]
+		public static extern uint midiOutGetNumDevs ();
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MIDIINCAPS {
+			public ushort wMid;
+			public ushort wPid;
+			public uint vDriverVersion;     // MMVERSION
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+			public string szPname;
+			public uint dwSupport;
+		}
+
+		public enum MMRESULT : uint {
+			MMSYSERR_NOERROR = 0,
+			MMSYSERR_ERROR = 1,
+			MMSYSERR_BADDEVICEID = 2,
+			MMSYSERR_NOTENABLED = 3,
+			MMSYSERR_ALLOCATED = 4,
+			MMSYSERR_INVALHANDLE = 5,
+			MMSYSERR_NODRIVER = 6,
+			MMSYSERR_NOMEM = 7,
+			MMSYSERR_NOTSUPPORTED = 8,
+			MMSYSERR_BADERRNUM = 9,
+			MMSYSERR_INVALFLAG = 10,
+			MMSYSERR_INVALPARAM = 11,
+			MMSYSERR_HANDLEBUSY = 12,
+			MMSYSERR_INVALIDALIAS = 13,
+			MMSYSERR_BADDB = 14,
+			MMSYSERR_KEYNOTFOUND = 15,
+			MMSYSERR_READERROR = 16,
+			MMSYSERR_WRITEERROR = 17,
+			MMSYSERR_DELETEERROR = 18,
+			MMSYSERR_VALNOTFOUND = 19,
+			MMSYSERR_NODRIVERCB = 20,
+			WAVERR_BADFORMAT = 32,
+			WAVERR_STILLPLAYING = 33,
+			WAVERR_UNPREPARED = 34
+		}
+
+		[DllImport("winmm.dll", SetLastError = true)]
+		public static extern MMRESULT midiInGetDevCaps (UIntPtr uDeviceID, ref MIDIINCAPS caps, uint cbMidiInCaps);
+
+		[StructLayout(LayoutKind.Sequential)]
+		public struct MIDIOUTCAPS {
+			public ushort wMid;
+			public ushort wPid;
+			public uint vDriverVersion;     //MMVERSION
+			[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+			public string szPname;
+			public ushort wTechnology;
+			public ushort wVoices;
+			public ushort wNotes;
+			public ushort wChannelMask;
+			public uint dwSupport;
+		}
+
+		// values for wTechnology field of MIDIOUTCAPS structure
+		private const ushort MOD_MIDIPORT = 1;     // output port
+		private const ushort MOD_SYNTH = 2;        // generic internal synth
+		private const ushort MOD_SQSYNTH = 3;      // square wave internal synth
+		private const ushort MOD_FMSYNTH = 4;      // FM internal synth
+		private const ushort MOD_MAPPER = 5;       // MIDI mapper
+		private const ushort MOD_WAVETABLE = 6;    // hardware wavetable synth
+		private const ushort MOD_SWSYNTH = 7;      // software synth
+
+		// flags for dwSupport field of MIDIOUTCAPS structure
+		private const uint MIDICAPS_VOLUME = 1;      // supports volume control
+		private const uint MIDICAPS_LRVOLUME = 2;    // separate left-right volume control
+		private const uint MIDICAPS_CACHE = 4;
+		private const uint MIDICAPS_STREAM = 8;      // driver supports midiStreamOut directly
+
+		[DllImport("winmm.dll", SetLastError = true)]
+		public static extern MMRESULT midiOutGetDevCaps (UIntPtr uDeviceID, ref MIDIOUTCAPS caps, uint cbMidiOutCaps);
+
+	}
+}
